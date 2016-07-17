c75a3f1f8f5f20589d89a2159914e5abd688115f
manuelj555 <programador.manuel@gmail.com>
2016-07-16 23:02:16 -0430

    Usando el componente Debug de Sf

--- public/index.php
+++ public/index.php
@@ -1,8 +1,11 @@
 <?php
 
+use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
 require __DIR__.'/../app/AppKernel.php';
+
+Debug::enable();
 
 $kernel = new AppKernel('dev', true);
 //$kernel = new AppKernel('prod', false);
