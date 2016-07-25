12c2511631c2d519c8ea334bf8eb6c4372dd795e
manuelj555 <programador.manuel@gmail.com>
2016-07-25 09:22:17 -0430

    Implementado bootstrap con bower, gulp y sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,46 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
- 
-gulp.task('default', function () {
-  return gulp.src('dev/css/app.css')
-  .pipe(autoprefixer({
-    browsers: ['last 2 versions'],
-    cascade: false
-  }))
-  .pipe(gulp.dest('dist/css'));
+var scss = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var uglifycss = require('gulp-uglifycss');
+var rewriteCSS = require('gulp-rewrite-css');
+
+gulp.task('js', function () {
+    return gulp.src([
+        'app/Resources/public/vendors/jquery/dist/jquery.min.js',
+        'app/Resources/public/vendors/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+        'app/Resources/public/js/**/*.js',
+    ])
+    .pipe(uglify())
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('public/compiled/js'));
 });
+
+gulp.task('css', function () {
+    var dest = 'public/compiled/css';
+
+    return gulp.src([
+        'app/Resources/public/css/*.scss',
+        'app/Resources/public/css/**/*.scss',
+    ])
+        .pipe(scss()).on('error', scss.logError)
+        .pipe(rewriteCSS({destination:dest}))
+        .pipe(concat('frontend.css'))
+        .pipe(uglifycss())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest(dest));
+});
+
+// Cuando ocurra algun cambio en .scss se ejecutara la tarea sass definida.
+gulp.task('css:watch', function() {
+    gulp.watch([
+        'app/Resources/public/css/*.scss',
+        'app/Resources/public/css/**/*.scss',
+    ], ['css']);
+})
+
+gulp.task('default', ['css', 'js']);
